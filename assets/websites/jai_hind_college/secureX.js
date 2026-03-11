db21da5dda653378e203d73756e21a9ef01f27a0
Laksh <lakshkukrejalk777@gmail.com>
2026-03-12 00:29:46 +0530

    Update and rename secureX.txt to secureX.js

---
+++ assets/websites/jai_hind_college/secureX.js
@@ -0,0 +1,11 @@
+// -------------------------------------------------------------------
+// Description : SecureX                                              
+// Author      : © Laksh Kukreja (@lakshkukreja07)                    
+// Updated     : Last On 12 March 2026 At 02:45AM [IST]               
+// License     : GNU General Public License V3 | Or Later             
+// License URI : https://www.gnu.org/licenses/gpl-3.0.en.html         
+// Copyright   : (CC) 2022-2026 Laksh Kukreja                         
+// GitHub URI  : https://www.github.com/lakshkukreja07                
+// -------------------------------------------------------------------
+
+window.JHC_SECURE={ACTIVATE_SECUREX:1}
